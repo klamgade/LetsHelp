6ffe0bf66009c7ee26c077ac421ba3d1412e20b8
kamal lamgade <kamal.lamgade@outlook.com>
2018-08-06 16:37:50 +1200

    added customer model properties, removed migration history table

--- DataAccess/Customer.cs
+++ DataAccess/Customer.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace DataAccess
 {
-    public class Class1
+    public class Customer
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "City required")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Contact person required")]
+        [DisplayName("Contact person")]
+        public string ContactPerson { get; set; }
+
+        [Required(ErrorMessage = "Telephone required")]
+        [DisplayName("Telephone")]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Notes { get; set; }
     }
 }
