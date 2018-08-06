389aef4a6062e32ff2e43899c4a390a870d05430
kamal lamgade <kamal.lamgade@outlook.com>
2018-08-06 15:58:15 +1200

    commit after creating DAL, customer class and dataContext

---
+++ Server/DataAccessLayer/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLayer
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "City required")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Contact person required")]
+        [DisplayName("Contact person")]
+        public string ContactPerson { get; set; }
+
+        [Required(ErrorMessage = "Telephone required")]
+        [DisplayName("Telephone")]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email required")]
+        [RegularExpression(@ "\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Wrong email format")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
---
+++ Server/DataAccessLayer/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    class DataContext: IdentityDbContext<Users>
+    {
+        public DataContext() : base("DefaultConnection") {}  
+
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
